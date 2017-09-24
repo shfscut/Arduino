7c14e1e77d2671c8b623474c3f366b1d99dd7df0
Nobody <nobody@yq150.onlinetools>
2017-09-24 12:43:59 +0800

    git commit by online edit file 1/handle.py

--- handle.py
+++ handle.py
@@ -1,4 +1,4 @@
-# -*- coding; utf-8 -*-
+# -*- coding: utf-8 -*-
 # filename: handle.py
 
 import hashlib
@@ -14,7 +14,7 @@ class Handle(object):
             timestamp = data.timestamp
             nonce = data.nonce
             echostr = data.echostr
-            token = "xxxx" #请按照公众平台官网\基本配置中信息填写
+            token = "helloshfscut"
 
             list = [token, timestamp, nonce]
             list.sort()
