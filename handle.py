b128bca5e3d70ae0877a0cea72e1d3c71eec437e
Nobody <nobody@yq150.onlinetools>
2017-09-24 22:33:27 +0800

    git commit by online edit file 1/handle.py

--- handle.py
+++ handle.py
@@ -40,7 +40,7 @@ class Handle(object):
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "test"
+                content = recMsg.Content
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
             else:
