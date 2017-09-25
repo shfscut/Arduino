83f4ddd8fa3e340db74ab8ef1a88c51cef86f04e
Nobody <nobody@yq150.onlinetools>
2017-09-25 13:03:51 +0800

    git commit by online edit file 1/handle.py

--- handle.py
+++ handle.py
@@ -44,47 +44,7 @@ class Handle(object):
                 fromUser = recMsg.ToUserName
                 content = ""
                 db=MySQLdb.connect(sae.const.MYSQL_HOST, sae.const.MYSQL_USER, sae.const.MYSQL_PASS, sae.const.MYSQL_DB)
-                cursor=db.cursor()
-                sql_query="SELECT * FROM switch where id=1"
-                sql_update_1="UPDATE switch set state=1 where id=1"
-                sql_update_0="UPDATE switch set state=0 where id=1"
-                arduino_id, arduino_state=None, None
-                try:
-                    cursor.execute(sql_query)
-                    results=cursor.fetchall()
-                    for row in results:
-                        arduino_id=row[0]
-                        arduino_state=row[1]
-                except:
-                    content = "Error: unable to fetch data"
-                if recMsg.Content=="open":
-                    if arduino_state != 1:
-                        
-                        try:
-                            cursor.execute(sql_update_1)
-                            db.commit()
-                        except:
-                            db.rollback()
-                elif recMsg.Content=="close":
-                    if arduino_state != 0:
-                        
-                        try:
-                            cursor.execute(sql_update_0)
-                            db.commit()
-                        except:
-                            db.rollback()
-                else:
-                    content = recMsg.Content
-                try:
-                    cursor.execute(sql_query)
-                    results=cursor.fetchall()
-                    for row in results:
-                        arduino_id=row[0]
-                        arduino_state=row[1]
-                    content = "arduino_state:"+str(arduino_state)
-                except:
-                    content = "Error: unable to fetch data"
-                db.close()
+                
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
             else:
